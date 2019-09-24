f6e0eee08c17dafdb77c943fe49dac01e85ec022
datsuns <the.skeleton7@gmail.com>
2019-09-24 14:19:35 +0900

    fix indentation by AndroidStudio

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // Import MaterialApp and other widgets which we can use to quickly create a material app
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:developer';
 
 void main() => runApp(new TodoApp());
 
@@ -7,8 +9,8 @@ class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Todo List',
-      home: new TodoList()
+        title: 'Todo List',
+        home: new TodoList()
     );
   }
 }
@@ -21,15 +23,34 @@ class TodoList extends StatefulWidget {
 class TodoListState extends State<TodoList> {
   List<String> _todoItems = [];
 
+  @override
+  void initState() {
+    super.initState();
+    _loadSavedData();
+    log('init');
+  }
+
+  void _loadSavedData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    for(var key in prefs.getKeys()){
+      await _todoItems.add(prefs.getString(key));
+    }
+  }
+
   // Instead of autogenerating a todo item, _addTodoItem now accepts a string
   void _addTodoItem(String task) {
+    //SharedPreferences prefs = await SharedPreferences.getInstance();
     // Putting our code inside "setState" tells the app that our state has changed,
     // and it will automatically re-render the list
     //             ^^^^^^^^^^^^^^^^^^^^^^^
     // ==> calling setState() to invoke TodoList.createState(),
     //     and that cause re-render
+    //debugPrint('add task ${task}');
     if( task.length > 0 ){
-      setState( () => _todoItems.add(task));
+      setState( () {
+        _todoItems.add(task);
+        //prefs.setString(task, task);
+      });
     }
   }
 
@@ -68,15 +89,16 @@ class TodoListState extends State<TodoList> {
   // Build the whole list of todo items
   Widget _buildTodoList() {
     return new ListView.builder(
-        // itemBuilder will be automatically be called as many times as it takes 
-        // for the list to fill up its available space, which is most likely 
-        // more than the number of todo items we have.
-        // So, we need to check the index is OK.
-        itemBuilder: (context, index) {
-          if(index < _todoItems.length){
-            return _buildTodoItem(_todoItems[index], index);
-          }
-        },
+      // itemBuilder will be automatically be called as many times as it takes
+      // for the list to fill up its available space, which is most likely
+      // more than the number of todo items we have.
+      // So, we need to check the index is OK.
+      // ignore: missing_return
+      itemBuilder: (context, index) {
+        if(index < _todoItems.length){
+          return _buildTodoItem(_todoItems[index], index);
+        }
+      },
     );
   }
 
@@ -92,13 +114,13 @@ class TodoListState extends State<TodoList> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Todo List')
+          title: new Text('Todo List')
       ),
 
       body: _buildTodoList(),
 
       floatingActionButton: new FloatingActionButton(
-          // pressing this button now opens the new screen
+        // pressing this button now opens the new screen
           onPressed: _pushAddTodoScreen,
           tooltip: 'Add task',
           child: new Icon(Icons.add)
@@ -109,29 +131,31 @@ class TodoListState extends State<TodoList> {
   void _pushAddTodoScreen() {
     // Push this page onto the stack
     Navigator.of(context).push(
-        // MaterialPageRoute will automatically animate the screen entry,
-        // as well as adding a back button to close it
+      // MaterialPageRoute will automatically animate the screen entry,
+      // as well as adding a back button to close it
         new MaterialPageRoute(
             builder: (BuildContext context) {
               return new Scaffold(
-                  appBar: new AppBar(
-                      title: new Text('Add new Task')
-                  ),
+                appBar: new AppBar(
+                    title: new Text('Add new Task')
+                ),
 
-                  body: new TextField(
-                      autofocus: true,
-                      onSubmitted: (val) {
-                        _addTodoItem(val);
-                        Navigator.pop(context); // Close the add todo screen
-                      },
-                      decoration: new InputDecoration(
-                                      hintText: 'Enter something todo ...',
-                                      contentPadding: const EdgeInsets.all(16.0)
-                                  ),
+                body: new TextField(
+                  autofocus: true,
+                  onSubmitted: (val) async {
+                    final SharedPreferences prefs = await SharedPreferences.getInstance();
+                    prefs.setString(val, val);
+                    _addTodoItem(val);
+                    Navigator.pop(context); // Close the add todo screen
+                  },
+                  decoration: new InputDecoration(
+                      hintText: 'Enter something todo ...',
+                      contentPadding: const EdgeInsets.all(16.0)
                   ),
+                ),
               );
             }
         )
-            );
+    );
   }
 }
